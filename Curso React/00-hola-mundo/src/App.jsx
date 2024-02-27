2f66c4de057dbbd137a5c46a234fe6b200632f86
Jhordan3010 <119618932+Jhordan3010@users.noreply.github.com>
2024-02-26 21:35:53 -0500

    ..

---
+++ Curso React/00-hola-mundo/src/App.jsx
@@ -0,0 +1,19 @@
+export function App(){
+    return(
+        <articule style={{display: 'flex', alignItems: 'center',color:'#fff'}}>
+            <header>
+                <img alt="El avatar" src="https://unavatar.io/Jhordan3010" />
+                <div>
+                    <strong>Jhordan Huera</strong>
+                    <span>@jhordan_huera</span>
+                </div>
+            </header>
+
+            <aside>
+                <button>
+                    Seguir
+                </button>
+            </aside>
+        </articule>
+    )
+}
