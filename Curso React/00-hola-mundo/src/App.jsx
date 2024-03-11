6edca049ca8d2d95ecf111cd7c2cab665e682825
Jhordan3010 <119618932+Jhordan3010@users.noreply.github.com>
2024-03-10 21:09:15 -0500

    nuevo

--- Curso React/00-hola-mundo/src/App.jsx
+++ Curso React/00-hola-mundo/src/App.jsx
@@ -1,11 +1,10 @@
 import './App.css'
 import { TwitterFollowCard } from './TwitterFollowCard.jsx'
 
-
 export function App(){
     return(
         <section className='App'>
-            <TwitterFollowCard isFollowing userName="jhordan3010" name="Jhordan Huera"/>
+            <TwitterFollowCard isFollowing userName="Jhordan3010" name="Jhordan Huera"/>
             <TwitterFollowCard isFollowing={false} userName="midudev" name="Miguel Ángel Durán"/>
             <TwitterFollowCard isFollowing userName="pheralb" name="Pablo Hernandez"/>
         </section>
